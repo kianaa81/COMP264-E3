66185c0db5eb083d9878941f03285c20c3a1e22b
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-01-25 02:19:03 -0600

    Updated the code

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const words = ["one", "two", "three"];
+    const moreWords = ["four", "five", "six"];
+    
+    const allWords = [...words, ...moreWords];
+    
+    document.write(allWords);
+
+    const name = "Muhammad";
+    const age = 25;
+    
+    const message = `Hello, my name is ${name}!
+    and I'm ${age} years old`;
+    
+    document.write(message);
